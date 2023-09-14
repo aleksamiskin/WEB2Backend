8fcb37d3986196dd9a6aa0c8a1c4a5f55793cfa4
Aleksa Miskin <aleksamiskin@gmail.com>
2023-09-14 13:53:24 +0200

    Change

---
+++ Prodavnica.Api/Infrastrucutre/ProdavnicaDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prodavnica.Api.Models;
+
+namespace Prodavnica.Api.Infrastructure
+{
+    public class ProdavnicaDbContext : DbContext
+    {
+        public ProdavnicaDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //Kazemo mu da pronadje sve konfiguracije u Assembliju i da ih primeni nad bazom
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProdavnicaDbContext).Assembly);
+            modelBuilder.Entity<Oreder>().HasKey(o => o.Id);;
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<ShoppingItem> ShoppingItems { get; set; }
+        public DbSet<Oreder> Orders { get; set; }
+    }
+}
